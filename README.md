33b1315a45a038305e34ad9bdd01da04f9b3b399
Lyne Malele <lynemalele@gmail.com>
2020-10-16 20:43:29 -0400

    Made minor changes

--- README.md
+++
@@ -1,4 +0,0 @@
-# employee-profile-summary
-
-# Description
-A software engineering team generator command line application that will prompt the user for information about the team members and create an HTML file that displays a nicely formatted team roster based on the information provided by the user.
--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
+//
+const teamMembers = [];
+const idArray = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
